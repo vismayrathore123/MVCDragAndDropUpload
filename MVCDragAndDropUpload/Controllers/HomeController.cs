07f1cefe682d9cb17ca0bce5d63f460acdb04d10
Vismay <vismayrathore2021@gmail.com>
2023-09-19 17:36:26 +0530

    Add project files.

---
+++ MVCDragAndDropUpload/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCDragAndDropUpload.Models;
+using System.Diagnostics;
+
+namespace MVCDragAndDropUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+       public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UploadFiles(IEnumerable<HttpPostedFileBase> files)
+        {
+            foreach (var file in files)
+            {
+                string filePath = Guid.NewGuid() + Path.GetExtension(file.FileName);
+                file.SaveAs(Path.Combine(Server.MapPath("~/UploadedFiles"), filePath));
+            
+            }
+            return Json("file uploaded succesfuuly");
+        } 
+        
+    }
+}
---
+++ MVCDragAndDropUpload/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCDragAndDropUpload.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
